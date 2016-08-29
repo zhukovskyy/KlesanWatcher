fad7851c7f06044400bfd300ee1bf66a972c90ea
poan_yang <poan_yang@trendmicro.com>
2016-08-29 22:18:01 +0800

    Add a script to getting data from cwb

---
+++ get_data.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+#set up your key for get wheater
+#ref: http://opendata.cwb.gov.tw/usages
+#export appkey=xxx-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+mkdir data
+
+for i in {1..9}
+do
+	if [ `echo "$i % 2" | bc`  -eq  1 ]
+	then
+		wget "http://opendata.cwb.gov.tw/opendataapi?dataid=F-D0047-00${i}&authorizationkey=${appkey}" -O "./data/F-D0047-00${i}"
+	fi
+done
+for i in {10..87}
+do
+	if [ `echo "$i % 2" | bc`  -eq  1 ]
+	then
+		wget "http://opendata.cwb.gov.tw/opendataapi?dataid=F-D0047-0${i}&authorizationkey=${appkey}" -O "./data/F-D0047-0${i}"
+	fi
+done
