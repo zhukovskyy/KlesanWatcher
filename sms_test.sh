1235fb6d4ea3a8dfd235fd68753c6cd53a2ca05a
poan_yang <poan_yang@trendmicro.com>
2016-10-01 23:35:56 +0800

    sms send

--- sms_test.sh
+++ sms_test.sh
@@ -1,3 +1,10 @@
-echo -en "AT+CMGF=1\r" > /dev/ttyUSB0
-echo -en "AT+CMGS=\"+886987026316\"\r" > /dev/ttyUSB0
-echo -en "test 天氣資訊\032" > /dev/ttyUSB0
+#!/usr/bin/sh
+if [ $# -eq 0 ]; then
+    echo "USAGE: sms <phone_num ex: +886987026316> <ascii_txt ex: Test message>"
+else
+    echo "sending test sms to ${1}"
+    echo -en "AT+CMGF=1\r" > /dev/ttyUSB2
+    #echo -en "AT+CSCA=\"${SMS_SERVICE_NUMBER}\",145\r" > /dev/ttyUSB2
+    echo -en "AT+CMGS=\"${1}\"\r" > /dev/ttyUSB2
+    echo -en "${2}\032" > /dev/ttyUSB2
+fi
