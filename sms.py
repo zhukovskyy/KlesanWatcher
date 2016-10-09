b4b29284954ca13c102dca99c4d3c1389fdae4fa
poan_yang <poan_yang@trendmicro.com>
2016-10-09 16:25:07 +0800

    get the sms api (script base)

--- sms.py
+++ sms.py
@@ -4,7 +4,12 @@ import logging
 
 def send(phone_number, msg):
     logging.info('SMS to {}: {}'.format(phone_number, msg))
-    return_code = subprocess.run(['./sms_test.sh', phone_number, msg])
-    if return_code:
+    result = subprocess.run(['./sms_test.sh', phone_number, msg])
+
+    # TODO:the script is just POC, the status of sms are not handled, this need to refactor by python code
+    if result.returncode:
+        return "OK"
+    else:
         logging.error('SMS to {} fail'.format(phone_number))
+        return "Fail", 503
 
